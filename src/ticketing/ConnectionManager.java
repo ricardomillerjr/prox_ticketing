4201beade05f5ab2cbbd26f242513a21a035f883
Ricardo Miller Jr <it.pro11@philhealth.gov.ph>
2018-11-06 20:11:27 +0800

    Update src/ticketing/ConnectionManager.java

--- src/ticketing/ConnectionManager.java
+++ src/ticketing/ConnectionManager.java
@@ -13,14 +13,10 @@ package ticketing;
 import ticketing.model.DBType;
 import java.io.FileInputStream;
 import java.io.IOException;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
 import java.util.Properties;
-
-import javax.swing.JOptionPane;
 
 public class ConnectionManager {
 
